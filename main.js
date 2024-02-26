1c060c031906836aab49c23f5bd018f65b7b2709
Anna Bargamotova <115473341+Bargamotova@users.noreply.github.com>
2024-02-27 00:34:03 +0900

    added main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let copyText = document.querySelector(".copy__text");
+copyText.querySelector("button").addEventListener('click', function () {
+  let input = copyText.querySelector("input.text");
+  input.ariaSelected();
+  document.execCommand("copy");
+  copyText.classList.add('active');
+  window.getSelection().removeAllRanges();
+  setTimeout(function () {
+    copyText.classList.remove('active');
+  }, 2000)
+});
