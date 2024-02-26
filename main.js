4e97a41e1b20de5231f6b056e2eebdd7201b5325
Anna Bargamotova <115473341+Bargamotova@users.noreply.github.com>
2024-02-27 01:12:51 +0900

    fix copy

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const copyText = document.querySelector(".copy__container");
 const copyBtn = document.querySelector('.copy__btn');
 
 copyBtn.addEventListener('click', function () {
-  let input = copyText.querySelector(".copy__text");
-  input.ariaSelected();
+  let input = document.querySelector(".copy__text");
+  input.select();
+
   document.execCommand("copy");
   copyText.classList.add('active');
   window.getSelection().removeAllRanges();
   setTimeout(function () {
     copyText.classList.remove('active');
-  }, 2000)
+  }, 2500)
 });
